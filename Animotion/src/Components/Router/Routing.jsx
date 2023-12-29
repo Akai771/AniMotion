28a5025656f6d6405b6309be7d00719be0e801e9
Akai <rishabh.hs@somaiya.edu>
2023-12-29 19:04:38 +0530

    changes BrowserRouter to HashRouter

--- Animotion/src/Components/Router/Routing.jsx
+++ Animotion/src/Components/Router/Routing.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import Signin from "../Signing/Signin";
 import Signup from "../Signing/Signup";
