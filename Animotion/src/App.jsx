943cf4647af2c706cdeb015099fdafcf805953a0
Akai <rishabh.hs@somaiya.edu>
2023-12-29 19:13:50 +0530

    vercel analytics

--- Animotion/src/App.jsx
+++ Animotion/src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 import Routing from "./Components/Router/Routing"
+import { inject } from '@vercel/analytics';
 
 function App() {
+  inject()
   return (
     <>
       <Routing/>
