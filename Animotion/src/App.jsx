579ae551622cf06b7c20eb9bd0dc63f0bfb02dec
Akai <rishabh.hs@somaiya.edu>
2024-01-12 17:51:23 +0530

    chore: install and configure Vercel Speed Insights

--- Animotion/src/App.jsx
+++ Animotion/src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react"
 import Routing from "./Components/Router/Routing"
 import { inject } from '@vercel/analytics';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   inject()
   return (
     <>
-      <SpeedInsights />
       <Routing/>
     </>
   )
--- Animotion/src/main.jsx
+++ Animotion/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <SpeedInsights />
     <App />
   </React.StrictMode>,
 )
