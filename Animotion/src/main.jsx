28a7a0920e252562fe323dfd21a258be06c3ec64
Akai <rishabh.hs@somaiya.edu>
2024-01-12 17:54:49 +0530

    Update main.jsx

--- Animotion/src/main.jsx
+++ Animotion/src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SpeedInsights />
     <App />
   </React.StrictMode>,
 )
