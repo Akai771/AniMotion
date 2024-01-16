55d811d46557c840f9747fb9cfb8958957c52cc4
Akai <rishabh.hs@somaiya.edu>
2024-01-16 15:12:18 +0530

    fix: resolve redirect issue on recommendation page

--- Animotion/src/Components/Video/VideoInfo/recommendCard.jsx
+++ Animotion/src/Components/Video/VideoInfo/recommendCard.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./recommendCard.css";
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 const RecommendCard = ({id, title, image}) => {
+    const navigate = useNavigate();
+    const handleClick = () =>{
+        navigate(`/details/${id}`)
+        window.location.reload();
+    }
     return (
       <>
         <div class="RecommendCard">
           <div className="RecommendImage">
-            <Link exact to={`/details/${id}`}>
+            <button className="recommendCardBtn" onClick={handleClick}>
                 <img className="RecommendCardImage" src={image} alt={title} />
                 <div className="playIcon2">
                   <PlayArrowIcon style={{fontSize:"2.8rem"}}/>
                 </div>
-            </Link>
+            </button>
           </div>
             <div class="RecommendCard-info">
               <span class="RecommendCardTitle">{title}</span>
