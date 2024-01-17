9923f9f0311e1d79eff2e3e3fa8500600c3b83c7
Akai <rishabh.hs@somaiya.edu>
2024-01-17 15:17:37 +0530

    feat: add new image source for Manga card

--- Animotion/src/Components/Merch/MangaCard/MangaCard.jsx
+++ Animotion/src/Components/Merch/MangaCard/MangaCard.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import "./MangaCard.css";
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import axios from "axios";
 
 const MangaCard = ({id, title, coverImage, vidUrl}) => {
+  const [mangaTitle, setMangaTitle] = React.useState([]);
+  useEffect(()=>{
+    axios.get(`https://animotion-consumet-api.vercel.app/manga/mangakakalot/${title}`)
+      .then((res) => setMangaTitle(res.data.results))
+    },[title])
+    const mangaImage = mangaTitle[0];
+
     return (
       <>
         <div class="VidCard">
           <div className="VidCardImage">
             <Link exact to={`/manga/details/${id}`}>
-                <img className="VidImage" src={coverImage} alt={title} />
+                <img className="VidImage" src={mangaImage?mangaImage.image :coverImage} alt={title} />
                 <div className="playIcon">
-                  <PlayArrowIcon style={{fontSize:"2.8rem"}}/>
+                  <AutoStoriesIcon style={{fontSize:"2.5rem"}}/>
                 </div>
             </Link>
           </div>
