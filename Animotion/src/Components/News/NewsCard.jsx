701f405fdd48509ffec7ef8a87662f30a4dc76f8
Akai <rishabh.hs@somaiya.edu>
2024-02-06 17:18:17 +0530

    style: adjust size of News Cards

--- Animotion/src/Components/News/NewsCard.jsx
+++ Animotion/src/Components/News/NewsCard.jsx
@@ -5,7 +5,7 @@ function NewsCard({ title, description, url, urlToImage}) {
     return(
         <>
             <div class="card cardColor mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
-            <a href={url}><img src={urlToImage?urlToImage:"https://intellij-support.jetbrains.com/hc/user_images/QZP8LIk0pW3bOuWt1P7HIQ.png"} style={{height:"200px", width:"100%"}} class="card-img-top" alt="NewsImage" /></a>
+            <a href={url}><img src={urlToImage?urlToImage:"https://intellij-support.jetbrains.com/hc/user_images/QZP8LIk0pW3bOuWt1P7HIQ.png"} style={{height:"300px", width:"100%"}} class="card-img-top" alt="NewsImage" /></a>
                 <div class="card-body">
                     <h5 class="card-title cardTitle">{title?title.slice(0,50):"NoData"}</h5>
                     {/* <p class="card-tex cardDesc">{description?description.slice(0,90):"some information about the news"}....</p> */}
